320b4818fc6a046d402e6e57b2e96549455214fe
Joyal <jpaul11@binghamton.edu>
2025-04-21 10:31:07 -0400

    updated

--- kivy_application/main.py
+++ kivy_application/main.py
@@ -10,6 +10,11 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 class MainWindow(Screen):
     def __init__(self, **kwargs):
+        '''
+        Initializes window layout and widget
+        args: None
+        return: None
+        '''
         super(MainWindow, self).__init__(**kwargs)
                 
         self.display = MainWidget(size_hint = (1, 0.8), pos_hint = {"y": 0.1})
@@ -26,6 +31,11 @@ class MainWindow(Screen):
         save.bind(on_press = self.switch_page)
         
     def switch_page(self, instance, file = "saves.json"):
+        '''
+        When switching pages, writes down everything into JSON
+        args: saves.json 
+        return: None
+        '''
         with open(file, "w") as my_doc:
             data = {date: text_input.text for date, text_input in self.display.replies_list.items()}
             json.dump(fp=my_doc, obj=data, indent=4)
@@ -35,24 +45,42 @@ class MainWindow(Screen):
         wm.current = "notepad"
 
     def write_back(self, file = "saves.json"):
+        '''
+        Pulls info from saves.json file and writes them down into calendar
+        args: saves.json
+        return: None
+        '''
         with open(file) as my_doc:
             data = json.load(my_doc)
             for date, text_input in self.display.replies_list.items():
                 text_input.text = data[str(date)]
         my_doc.close()
+ 
         
 def first_again():
-        first_list = []
-        month = time.localtime().tm_mon
-        for _ in range(3):
-            updated = time.strptime(f"{time.localtime().tm_year}-{month:02d}-01", "%Y-%m-%d").tm_wday
-            first_list.append(updated)
-            month += 1
-        return first_list
+    '''
+    Finds First Day of monts
+    args: None
+    return: List of integers representing first day of the month for next 3 months
+    '''
+    first_list = []
+    month = time.localtime().tm_mon
+    for _ in range(3):
+        updated = time.strptime(f"{time.localtime().tm_year}-{month:02d}-01", "%Y-%m-%d").tm_wday
+        first_list.append(updated)
+        month += 1
+    return first_list
 vals = first_again()
+
+
         
 class MainWidget(GridLayout):
     def __init__(self, **kwargs):
+        '''
+        Initializes mainwidget layout and widgets that appear under mainwindow
+        args: None
+        return: None
+        '''
         super(MainWidget, self).__init__(**kwargs)
         self.cols = 7
         date = 1
@@ -77,36 +105,44 @@ class MainWidget(GridLayout):
             date += 1
 
     def first(self, month = time.localtime().tm_mon):
+        '''
+        Initializes layout and widget
+        args: month
+        return: Integer representing first day of week
+        '''
         updated = time.strptime(f"{time.localtime().tm_year}-{month:02d}-01", "%Y-%m-%d").tm_wday
+        print("Updated", updated)
         return updated
 
     def indent(self):
+        '''
+        Adds indent based on first day of month, meaning if Sunday there is no indent, and if another day, 
+        moves forward necessary columns
+        args: None
+        return: None
+        '''
         self.first_day = vals[0]
         if self.first_day != 6:
             for _ in range(self.first_day + 1):
                 self.add_widget(Label())
                 
     def updated(self):
+        '''
+        Updates to first day
+        args: None
+        return: None
+        '''
         self.first_day = vals[1]
                 
-class Second_Month(Screen):
-    def __init__(self, **kwargs):
-        super(Second_Month, self).__init__(**kwargs)
-        
-        self.display = MainWidget(size_hint = (1, 0.8), pos_hint = {"y": 0.1})
-        self.add_widget(self.display)
-        
-        self.add_widget(Label(text = f"{self.display.months[time.localtime().tm_mon]}",
-                              font_size = 45, 
-                              size_hint = (1, 0.1), 
-                              pos_hint = {'top': 1}))
-        save = Button(text = "Save", size_hint = (1, 0.1), pos_hint = {'bottom': 0}, font_size = 35)
-        self.add_widget(save)
-        
-        
+
         
 class Notepad(Screen):
     def __init__(self, **kwargs):
+        '''
+        Initializes layout and widget
+        args: None
+        return: None
+        '''
         super(Notepad, self).__init__(**kwargs)
         self.add_widget(Label(text = "Notepad [Won't Save]", font_size = 40, size_hint = (1, 0.1), pos_hint = {"top": 1}))
         self.add_widget(TextInput(size_hint = (1, 0.9), pos_hint = {"bottom": 0}))
@@ -115,20 +151,26 @@ class Notepad(Screen):
         back.bind(on_press = self.go_back)
         
     def go_back(self, instance):
+        '''
+        Function to return to prevous page
+        args: None
+        return: None
+        '''
         wm.transition.direction = "left"
-        wm.current = "main"
-    
-    
-    
+        wm.current = "main"       
+  
+
+# Initializing and running app using screenmanager  
 wm = ScreenManager()
 mw = MainWindow(name = "main")
 wm.add_widget(mw)
 np = Notepad(name = "notepad")
 wm.add_widget(np)
 
+
 class CalendarApp(App):
     def build(self):
-        return Second_Month()
+        return wm
 
 
 if __name__ == '__main__':
